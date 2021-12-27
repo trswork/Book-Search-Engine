25133c23ca38b508f413fb0d7550df6aa490f5a1
trswork <87744214+trswork@users.noreply.github.com>
2021-12-26 23:07:57 -0500

    update

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import Auth from '../../utils/auth';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
