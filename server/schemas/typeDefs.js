fd3c089615d876e4bb27f340f368967577f43932
trswork <87744214+trswork@users.noreply.github.com>
2021-12-27 00:59:23 -0500

    update

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      saveBook(author: [String], description: String, title:  String, bookId: String, image: String, link: String): User
+      removeBook(bookId: ID): User
+  }
+
+  type User {
+      _id: ID
+      username: String
+      email: String
+      bookCount: Int
+      savedBooks: [Book]
+  }
+
+  type Book {
+      bookId: ID
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+  }
+  
+  type Auth {
+      token: ID!
+      user: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
