492860fe0daeac7781fd429a454f1307f6d10ec1
trswork <87744214+trswork@users.noreply.github.com>
2022-01-12 22:08:18 -0500

    update

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Auth from '../../utils/auth';
+
 
 ReactDOM.render(
   <React.StrictMode>
